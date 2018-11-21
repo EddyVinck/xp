ee6a58526a8b28636522e8a50264d8e7543f0324
EddyVinck <eddyvinck95@gmail.com>
2018-11-21 21:22:11 +0100

    finish getFileUrl, add more control flow for minimzing and unminimizing, add icons to folder window

--- js/File.js
+++ js/File.js
@@ -1,4 +1,4 @@
-import imageUrls from "../img/*.*";
+import getIconUrl from "./utils/getIconUrl.js";
 import { el, mount } from "redom";
 
 const wallpaperGrid = document.querySelector(".wallpaper-grid");
@@ -8,14 +8,12 @@ class File {
   constructor({
     name = "Unnamed",
     type = "folder",
-    parentFolder = wallpaperGrid,
-    innerFolders = [],
+    parentElement = wallpaperGrid,
     innerFiles = []
   } = {}) {
     this.name = name;
     this.type = type;
-    this.parentFolder = parentFolder;
-    this.innerFolders = innerFolders;
+    this.parentElement = parentElement;
     this.innerFiles = innerFiles;
 
     // A lovely collection of boolean flags (for now)
@@ -31,44 +29,46 @@ class File {
 
     // Function binding
     this.remove = this.remove.bind(this);
-    this.openWindow = this.openWindow.bind(this);
+    this.showWindow = this.showWindow.bind(this);
+    this.showTaskbarCell = this.showTaskbarCell.bind(this);
     this.closeWindow = this.closeWindow.bind(this);
-    this.createNecessaryElements = this.createNecessaryElements.bind(this);
+    this.addFileToParentElement = this.addFileToParentElement.bind(this);
     this.handleDesktopSingleClick = this.handleDesktopSingleClick.bind(this);
     this.handleDesktopDoubleClick = this.handleDesktopDoubleClick.bind(this);
     this.handleTaskbarClick = this.handleTaskbarClick.bind(this);
 
-    // Setup functions
-    this.createNecessaryElements();
+    this.addFileToParentElement();
   }
 
   // Creates desktop file, taskbar item
   // Adds necessary event listeners
-  createNecessaryElements() {
+  addFileToParentElement() {
     // Create the file on the desktop or folder
     this.desktopElement = createDesktopElement(this.name, this.type);
-    this.parentFolder.appendChild(this.desktopElement);
+    this.parentElement.appendChild(this.desktopElement);
     this.desktopElement.addEventListener("click", this.handleDesktopSingleClick);
     this.desktopElement.addEventListener("dblclick", this.handleDesktopDoubleClick);
-
-    // create the opened window
   }
 
   remove() {
     this.taskbarElement.removeEventListener(this.handleTaskbarClick);
     this.desktopElement.removeEventListener(this.handleDesktopSingleClick);
-    this.parentFolder.removeChild(this.desktopElement);
+    this.parentElement.removeChild(this.desktopElement);
   }
 
   handleTaskbarClick(e) {
     console.log("taskbar click", this);
     const { isOpen } = this.state;
+    console.log(`was this item already opened? ${isOpen}`);
 
     if (isOpen) {
-      // minify it
+      // minimize it
+      console.log("minimizing...");
     } else {
+      console.log("opening...");
+
       // open the window
-      this.openWindow();
+      this.showWindow();
     }
 
     this.state.isOpen = !isOpen;
@@ -77,29 +77,15 @@ class File {
   handleDesktopSingleClick(e) {}
 
   handleDesktopDoubleClick(e) {
-    if (this.taskbarElement instanceof HTMLElement) {
-      // taskbarElement already created
-      // no need to do that again
-    } else {
-      this.openInital();
-    }
-    this.openWindow();
-  }
+    const { isOpen } = this.state;
+    this.state.isOpen = !isOpen;
 
-  // Opens a file that is currently not opened
-  // create the taskbar element
-  // and open the window
-  openInital() {
-    // Create taskbarElement
-    const taskbarElement = createTaskbarElement(this.desktopElement.cloneNode(true));
-    this.taskbarElement = taskbarElement;
-    taskbar.appendChild(taskbarElement);
-    this.taskbarElement.addEventListener("click", this.handleTaskbarClick);
+    this.showTaskbarCell();
+    this.showWindow();
   }
 
   // Open the file's window
-  openWindow() {
-    console.log("opening window element...");
+  showWindow() {
     let windowElement = null;
 
     if (this.windowElement instanceof HTMLElement) {
@@ -109,14 +95,31 @@ class File {
       // windowElement did not exist yet
       windowElement = createWindowElement(this.name, this.type);
       this.windowElement = windowElement;
-      console.log(this.windowElement);
       this.windowElement.querySelector(".close").addEventListener("click", this.closeWindow);
     }
     document.body.appendChild(windowElement);
   }
 
+  // Put the file in the taskbar
+  showTaskbarCell() {
+    let taskbarElement = null;
+
+    if (this.taskbarElement instanceof HTMLElement) {
+      taskbarElement = this.taskbarElement;
+    } else {
+      // Create the taskbar element because it didnt exist
+      taskbarElement = createTaskbarElement(this.desktopElement.cloneNode(true));
+      this.taskbarElement = taskbarElement;
+      this.taskbarElement.addEventListener("click", this.handleTaskbarClick);
+    }
+    taskbar.appendChild(taskbarElement);
+  }
+
   closeWindow() {
+    this.state.isOpen = false;
+
     this.windowElement.parentNode.removeChild(this.windowElement);
+    this.taskbarElement.parentNode.removeChild(this.taskbarElement);
   }
 }
 
@@ -140,7 +143,7 @@ function createDesktopElement(fileName, fileType) {
   desktopElement.classList.add("cell");
 
   const img = document.createElement("img");
-  img.src = getFileIconUrl(fileType);
+  img.src = getIconUrl(fileType);
   img.alt = fileType;
 
   const cellName = document.createElement("span");
@@ -151,23 +154,6 @@ function createDesktopElement(fileName, fileType) {
   desktopElement.appendChild(cellName);
 
   return desktopElement;
-}
-
-function getFileIconUrl(fileIconName) {
-  let fileIconUrl = "";
-  switch (fileIconName) {
-    case "folder":
-      fileIconUrl += Object.values(imageUrls["folder-empty"])[0];
-      break;
-    case "control-panel":
-      fileIconUrl += Object.values(imageUrls["control-panel"])[0];
-      break;
-    default:
-      fileIconUrl += Object.values(imageUrls["folder-empty"])[0];
-      break;
-  }
-
-  return fileIconUrl;
 }
 
 // Creates and returns the window HTML element
@@ -214,10 +200,14 @@ function createWindowElement(fileName, fileType) {
       ".folder-header",
       el(
         ".top-bar",
-        el(".folder-label", el("img", { src: getFileIconUrl(fileType), alt: fileName }), el("span", fileName)),
+        el(
+          ".folder-label",
+          el("img", { src: getIconUrl(fileType), alt: fileName }),
+          el("span", fileName)
+        ),
         el(
           ".top-bar-controls",
-          el(".top-bar-button.minimze"),
+          el(".top-bar-button.minimize"),
           el(".top-bar-button.maximize"),
           el(".top-bar-button.close")
         )
@@ -225,7 +215,26 @@ function createWindowElement(fileName, fileType) {
       el(
         ".menu-bar",
         el(".menu-top"),
-        el(".menu-bottom"),
+        el(
+          ".menu-bottom",
+          el(
+            "ul",
+            el(
+              "li",
+              el("img", { src: getIconUrl("arrow-back"), alt: "back icon" }),
+              el("span", "Back")
+            ),
+            el("li", el("img", { src: getIconUrl("arrow-next"), alt: "next icon" }))
+          ),
+          el(
+            "ul",
+            el(
+              "li",
+              el("img", { src: getIconUrl("search"), alt: "Search icon" }),
+              el("span", "Search")
+            )
+          )
+        ),
         el(
           ".address-bar",
           el("span", "Address"),
@@ -233,7 +242,7 @@ function createWindowElement(fileName, fileType) {
             ".address-input-wrapper",
             el(
               ".address-input-icon",
-              el("img", { src: getFileIconUrl(fileType), alt: fileType }),
+              el("img", { src: getIconUrl(fileType), alt: fileType }),
               el("input.address-input", {
                 type: "text",
                 value: "C:\\Desktop\\Folder Name",
