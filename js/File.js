658e1f62178900d48b4558ba289662eaa330c990
EddyVinck <eddyvinck95@gmail.com>
2018-11-26 16:21:25 +0100

    resize works, but makes the window jump around after moving it

--- js/File.js
+++ js/File.js
@@ -94,6 +94,41 @@ class File {
         ignoreFrom: ".minimize, .maximize, .close",
         onmove: this.handleDrag
       });
+
+      interact(this.windowElement)
+        .resizable({
+          // resize from all edges and corners
+          edges: { left: true, right: true, bottom: true, top: true },
+
+          // keep the edges inside the parent
+          restrictEdges: {
+            outer: "parent",
+            endOnly: true
+          },
+
+          // minimum size
+          restrictSize: {
+            min: { width: 100, height: 50 }
+          }
+        })
+        .on("resizemove", function(event) {
+          var target = event.target,
+            x = parseFloat(target.getAttribute("data-x")) || 0,
+            y = parseFloat(target.getAttribute("data-y")) || 0;
+
+          // update the element's style
+          target.style.width = event.rect.width + "px";
+          target.style.height = event.rect.height + "px";
+
+          // translate when resizing from top or left edges
+          x += event.deltaRect.left;
+          y += event.deltaRect.top;
+
+          target.style.transform = "translate(" + x + "px," + y + "px)";
+
+          target.setAttribute("data-x", x);
+          target.setAttribute("data-y", y);
+        });
     }
     document.body.appendChild(windowElement);
   }
