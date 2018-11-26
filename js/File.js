b2495aea3442d16034ca8f9bbfa6faa6761d06ed
EddyVinck <eddyvinck95@gmail.com>
2018-11-26 16:55:03 +0100

    fix the window jump when resizing after dragging

--- js/File.js
+++ js/File.js
@@ -44,6 +44,7 @@ class File {
     this.handleDesktopDoubleClick = this.handleDesktopDoubleClick.bind(this);
     this.handleTaskbarClick = this.handleTaskbarClick.bind(this);
     this.handleDrag = this.handleDrag.bind(this);
+    this.handleResize = this.handleResize.bind(this);
 
     this.addFileToParentElement();
   }
@@ -111,24 +112,7 @@ class File {
             min: { width: 100, height: 50 }
           }
         })
-        .on("resizemove", function(event) {
-          var target = event.target,
-            x = parseFloat(target.getAttribute("data-x")) || 0,
-            y = parseFloat(target.getAttribute("data-y")) || 0;
-
-          // update the element's style
-          target.style.width = event.rect.width + "px";
-          target.style.height = event.rect.height + "px";
-
-          // translate when resizing from top or left edges
-          x += event.deltaRect.left;
-          y += event.deltaRect.top;
-
-          target.style.transform = "translate(" + x + "px," + y + "px)";
-
-          target.setAttribute("data-x", x);
-          target.setAttribute("data-y", y);
-        });
+        .on("resizemove", this.handleResize);
     }
     document.body.appendChild(windowElement);
   }
@@ -150,6 +134,28 @@ class File {
         y
       };
     }
+  }
+
+  handleResize(event) {
+    const target = event.target;
+    let x = this.state.position.x || 0;
+    let y = this.state.position.y || 0;
+
+    // update the element's style
+    target.style.width = event.rect.width + "px";
+    target.style.height = event.rect.height + "px";
+
+    // translate when resizing from top or left edges
+    x += event.deltaRect.left;
+    y += event.deltaRect.top;
+
+    this.moveWindow(x, y);
+
+    // update the posiion state
+    this.state.position = {
+      x,
+      y
+    };
   }
 
   moveWindow(x, y) {
