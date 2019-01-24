df7f9e1e1357953e242512e0ecfe071ff198c59c
EddyVinck <eddyvinck95@gmail.com>
2019-01-24 21:12:36 +0100

    fix tests

---
+++ src/js/__tests__/example.ts
@@ -0,0 +1,11 @@
+import coalesce from '../utils/coalesce';
+
+test("it works", () => {});
+
+test("check ENV", () => {
+  expect(process.env.NODE_ENV).toBe("test");
+});
+
+test("coalesce works", () => {
+  expect(coalesce(null, undefined, "maybe")).toBe("maybe");
+});
