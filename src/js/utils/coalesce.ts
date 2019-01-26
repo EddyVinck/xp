46e8ab00ae93cb589024b19def7c70d8841208d6
EddyVinck <eddyvinck95@gmail.com>
2019-01-26 15:52:41 +0100

    replace any[] with unknown[]

--- src/js/utils/coalesce.ts
+++ src/js/utils/coalesce.ts
@@ -1,16 +1,15 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 type ExcludeNullIfAnyNotNullHelper<T> = {
   [P in keyof T]-?:
     | (null extends T[P] ? never : null)
     | (undefined extends T[P] ? never : undefined)
 }[keyof T];
-type ExcludeNullIfAnyNotNull<T extends any[]> = Exclude<
+type ExcludeNullIfAnyNotNull<T extends unknown[]> = Exclude<
   T[number],
   ExcludeNullIfAnyNotNullHelper<T>
 >;
 
-function coalesce<T extends any[]>(...args: T): ExcludeNullIfAnyNotNull<T>;
-function coalesce<T extends any[]>(...args: T): T[number] {
+function coalesce<T extends unknown[]>(...args: T): ExcludeNullIfAnyNotNull<T>;
+function coalesce<T extends unknown[]>(...args: T): T[number] {
   for (let i = 0; i < args.length; i++) {
     // args[i] === args[i] is to avoid NaN, because NaN !== NaN
     if (args[i] != null && args[i] === args[i]) {
@@ -19,6 +18,5 @@ function coalesce<T extends any[]>(...args: T): T[number] {
   }
   return null;
 }
-/* eslint-enable @typescript-eslint/no-explicit-any */
 
 export default coalesce;
